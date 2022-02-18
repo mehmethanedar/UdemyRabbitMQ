2a9711f14462b05c09d781cc7f5521efec47a705
Mehmet HANEDAR <mehmethanedar@hotmail.com>
2022-02-18 16:53:00 +0300

    Initial

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);//durable => uygulama restart olduğunda exchange'lerimiz kaybolmaması için
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);//durable => uygulama restart olduğunda exchange'lerimiz kaybolmaması için
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"log {x}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+                Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout); //yazmasak da olur 
+
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+
+            //channel.QueueDeclare(randomQueueName, true, false, false);
+
+            channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName, false, consumer);
+
+            Console.WriteLine("Loglar dinleniyor..");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Gelen Mesaj:" + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
